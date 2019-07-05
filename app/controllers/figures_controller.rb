b313d1bad938e96b6d0ad57d81f5faf9203fc220
tanfly <mcewen.stella@gmail.com>
2019-07-04 22:00:22 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,47 @@
 class FiguresController < ApplicationController
-  # add controller methods
-end
+  get '/figures' do
+    @figures = Figure.all
+     erb :"figures/index"
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+     redirect "/figures"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @titles = @figure.titles
+    @landmarks = @figure.landmarks
+    erb :"figures/show"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/edit"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    @figure.save
+    redirect to "figures/#{figure.id}"
+  end
+end	
